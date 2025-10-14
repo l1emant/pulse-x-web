97f26f4759f8f65ee6a2536da8a007d3fbcbe7e9
l1emant <91789840+l1emant@users.noreply.github.com>
2025-10-15 01:32:00 +0530

    Add Features, CTA, FAQs, Footer sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import HeroSection from "@/components/hero-section";
+import Features from "@/components/features";
+import Footer from "@/components/footer";
+import CallToAction from "@/components/call-to-action";
+import Faqs from "@/components/faqs";
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <CallToAction />
+      <Faqs />
+      <Footer />
+    </>
+  );
 }
